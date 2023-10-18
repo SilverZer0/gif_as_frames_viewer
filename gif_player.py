6e5ec7cbaf1f30ba882dfa4f659c8b1d2ee8105a
SilverZer0 <101376364+SilverZer0@users.noreply.github.com>
2023-10-18 22:52:07 +0200

    made it possible to use when imported

--- gif_player.py
+++ gif_player.py
@@ -4,11 +4,20 @@ from tkinter import filedialog as fd
 import threading, time, sys
 
 MAX_HEIGHT = 950
-class GIFPlayer:
-    def __init__(self, path):
+class GIFPlayer(tk.Tk):
+    def __init__(self, files):
+        super().__init__()
+        self.files = list(files)
+        
+        self.auto = tk.BooleanVar(value = False)
+        self.speed = tk.DoubleVar(value = 1.0)
+        self.new(self.files.pop(0))
+
+    def new(self, path):
+        self.geometry(f'{300}x{80}+0+0')        
         self.lock = True
         self.path = path
-        imageObject = Image.open(path)
+        imageObject = Image.open(self.path)
         if hasattr(imageObject, 'n_frames'):
             self.max_frame = imageObject.n_frames - 1
         else:
@@ -25,65 +34,63 @@ class GIFPlayer:
             }
         }
 
-        self.root = tk.Tk()
+        self.title(f'Play '+self.path.replace('\\','/').split('/')[-1])
+        self.geometry(f'{max(self.size[0], 300)}x{self.size[1]+80}+0+0')
+        self.focus_force()
+
         self.build()
         self.setup()
 
     def build(self):
-        self.root.title(f'Play '+self.path.replace('\\','/').split('/')[-1])
-        self.root.geometry(f'{max(self.size[0], 300)}x{self.size[1]+80}+0+0')
-        self.root.focus_force()
-        
         self.index = tk.IntVar(value=0)
         self.index.trace_add('write', self.setup)
         self.step = tk.IntVar(value=max(1, round((self.max_frame+1)/100)))
-        self.auto = tk.BooleanVar(value = False)
         self.time = tk.StringVar(value = "")
-        self.speed = tk.DoubleVar(value = 1.0)
+        self.index_text = tk.StringVar(value = f'out of {str(self.max_frame):<5}')
 
         self.img_l = tk.Label()
-        self.img_l.grid(row=1, column = 0, columnspan=6)
+        self.img_l.grid(row=0, column = 0, columnspan=6)
 
-        minus_b = tk.Button(self.root, text='-'.center(15), command=self.minus)
-        index_e = tk.Entry(self.root, textvariable=self.index, width=7,
-                        validate = 'key', validatecommand=(self.root.register(self.nums), '%P'))
-        index_l = tk.Label(text=f'out of {self.max_frame}')
+        minus_b = tk.Button(self, text='-'.center(15), command=self.minus)
+        index_e = tk.Entry(self, textvariable=self.index, width=7,
+                        validate = 'key', validatecommand=(self.register(self.nums), '%P'))
+        index_l = tk.Label(textvariable=self.index_text)
         step_l = tk.Label(text=f'step=')
-        step_e = tk.Entry(self.root, textvariable=self.step, width=7,
-                        validate = 'key', validatecommand=(self.root.register(self.nums), '%P'))
-        plus_b = tk.Button(self.root, text='+'.center(15), command=self.plus)
+        step_e = tk.Entry(self, textvariable=self.step, width=7,
+                        validate = 'key', validatecommand=(self.register(self.nums), '%P'))
+        plus_b = tk.Button(self, text='+'.center(15), command=self.plus)
 
-        minus_b.grid(row=5, column=0)
-        index_e.grid(row=5, column=1, sticky='E')
-        index_l.grid(row=5, column=2, sticky='W')
-        step_l.grid(row=5, column=3, sticky='E')
-        step_e.grid(row=5, column=4, sticky='W')
-        plus_b.grid(row=5, column=5)
+        minus_b.grid(row=1, column=0)
+        index_e.grid(row=1, column=1, sticky='E')
+        index_l.grid(row=1, column=2, sticky='W')
+        step_l.grid(row=1, column=3, sticky='E')
+        step_e.grid(row=1, column=4, sticky='W')
+        plus_b.grid(row=1, column=5)
 
         auto_l = tk.Label(text=f'autoplay')
-        auto_c = tk.Checkbutton(self.root, variable=self.auto, onvalue=True, offvalue=False)
+        auto_c = tk.Checkbutton(self, variable=self.auto, onvalue=True, offvalue=False)
         time_l = tk.Label(textvariable = self.time)
         speed_l = tk.Label(text=f'speed=')
-        speed_e = tk.Entry(self.root, textvariable=self.speed, width=7,
-                        validate = 'key', validatecommand=(self.root.register(self.floats), '%P'))
+        speed_e = tk.Entry(self, textvariable=self.speed, width=7,
+                        validate = 'key', validatecommand=(self.register(self.floats), '%P'))
 
-        auto_l.grid(row=6, column=0, sticky='E')
-        auto_c.grid(row=6, column=1, sticky='W')
-        time_l.grid(row=6, column=2, columnspan=2)
-        speed_l.grid(row=6, column=4, sticky='E')
-        speed_e.grid(row=6, column=5, sticky='W')
+        auto_l.grid(row=2, column=0, sticky='E')
+        auto_c.grid(row=2, column=1, sticky='W')
+        time_l.grid(row=2, column=2, columnspan=2)
+        speed_l.grid(row=2, column=4, sticky='E')
+        speed_e.grid(row=2, column=5, sticky='W')
 
-        self.root.bind('<Left>', self.minus)
-        self.root.bind('<Right>', self.plus)
-        self.root.bind('<Up>', self.step_plus)
-        self.root.bind('<Down>', self.step_minus)
-        self.root.bind('<Escape>', lambda *_:self.root.destroy())
-        self.root.bind('<Delete>', lambda *_:self.root.destroy())
-        self.root.bind('a', lambda *_:self.auto.set(not self.auto.get()))
-        self.root.bind('s', lambda *_:self.speed.set(1.0))
-        self.root.bind('d', lambda *_:self.speed.set(2.0))
-        self.root.bind('f', lambda *_:self.speed.set(5.0))
-        self.root.bind('h', lambda *_:self.speed.set(0.5))
+        self.bind('<Left>', self.minus)
+        self.bind('<Right>', self.plus)
+        self.bind('<Up>', self.step_plus)
+        self.bind('<Down>', self.step_minus)
+        self.bind('<Escape>', lambda *_:self.kill())
+        self.bind('<Delete>', lambda *_:self.kill())
+        self.bind('a', lambda *_:self.auto.set(not self.auto.get()))
+        self.bind('s', lambda *_:self.speed.set(1.0))
+        self.bind('d', lambda *_:self.speed.set(2.0))
+        self.bind('f', lambda *_:self.speed.set(5.0))
+        self.bind('h', lambda *_:self.speed.set(0.5))
 
     def setup(self,*_):
         global imgTk #this needes to be global (why?)
@@ -103,14 +110,15 @@ class GIFPlayer:
         self.imgs[_index]['thread'] = None
         imgTk = ImageTk.PhotoImage(self.imgs[_index]['img'])
         time.sleep(0.03)
+        self.img_l.forget()
         _label = tk.Label(image=imgTk)
-        _label.grid(row=1, column=0, columnspan=6)
+        _label.grid(row=0, column=0, columnspan=6)
         self.update_time(_index, time.time()*1000, self.imgs[_index]['len'])
         time.sleep(0.01)
         self.img_l.forget()
         self.img_l = _label
 
-        self.root.after_idle(self.unlock)
+        self.after_idle(self.unlock)
 
     def plus(self, *_):
         if self.lock: return
@@ -160,12 +168,11 @@ class GIFPlayer:
     def save_frame_thread(self, path, idx):
         try:
             imageObject = Image.open(path)
-            if not (hasattr(imageObject, 'n_frames') or idx == 0) \
-            or idx < 0 or idx >= imageObject.n_frames:
+            if idx < 0 or idx >= (imageObject.n_frames if hasattr(imageObject, 'n_frames') else 1):
                 return
             imageObject.seek(idx)
             self.imgs[idx]['img'] = imageObject.resize(self.size)
-            self.imgs[idx]['len'] = imageObject.info['duration']
+            self.imgs[idx]['len'] = imageObject.info.get('duration', 0)
             time.sleep(0.01)
         except Exception as e:
             print(f'Frame {idx} failed: {e}')
@@ -191,9 +198,17 @@ class GIFPlayer:
                 self.plus()
             return
         self.time.set(f'{str(y-x):>7} of {str(y):>6} ({f"{(x)/(y or 1)*100:0.2f}":>7}%)')
-        self.root.after(100,lambda: self.update_time(index, start, length))
+        self.after(100,lambda: self.update_time(index, start, length))
+
+    def kill(self):
+        self.img_l.destroy()
+        if self.files:
+            self.new(self.files.pop(0))
+        else:
+            self.destroy()
 
 if __name__ == '__main__':
+    GP = None
     print('''
 ## TODO ##
 # add mp4 (/video) mode
@@ -208,9 +223,11 @@ if __name__ == '__main__':
         if len(sys.argv) > 1:
             files = sys.argv[1:]
         else:
-            files = fd.askopenfilenames(filetypes=[('GIF', '.gif')])
-        for i in files:
-            GP = GIFPlayer(i)
-            GP.root.mainloop()
+            files = fd.askopenfilenames(filetypes=[('GIF', '.gif'), ('Any', '*')])
+        GP = GIFPlayer(files)
+        GP.mainloop()
     except Exception as e:
         input(e)
+    finally:
+        if 0 and not GP is None:
+            GP.destroy()
